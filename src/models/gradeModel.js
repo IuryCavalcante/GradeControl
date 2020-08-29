c8cba8c6180da1dc1062fe4ee65ad4ee671864ee
Iury <iury.cavalcante@gmail.com>
2020-08-29 11:03:39 -0300

    first commit

---
+++ src/models/gradeModel.js
@@ -0,0 +1,28 @@
+export default (mongoose) => {
+  const schema = mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    lastModified: {
+      type: Date,
+      required: true,
+    },
+  });
+
+  const model = mongoose.model('grades', schema);
+
+  return model;
+};
